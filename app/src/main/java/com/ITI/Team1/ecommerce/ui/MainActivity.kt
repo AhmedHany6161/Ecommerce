1371526752471dd5a1c98ee38fe58db47275137f
ahmed <ahmed.hany.6161@gmail.com>
2021-05-20 23:03:18 +0200

    init Struct

---
+++ app/src/main/java/com/ITI/Team1/ecommerce/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ITI.Team1.ecommerce.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.ITI.Team1.ecommerce.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
