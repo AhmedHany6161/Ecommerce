86b23d4c350d6cb129d73f0d4ccb35d46db7e8e6
Wesam Elmeniawy <wesam.elmeniawy@gmail.com>
2021-05-23 18:47:48 +0200

    build card details design

--- app/src/main/java/com/iti/team/ecommerce/ui/product_details/ProductDetailsFragment.kt
+++ app/src/main/java/com/iti/team/ecommerce/ui/product_details/ProductDetailsFragment.kt
@@ -7,16 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.iti.team.ecommerce.R
+import com.iti.team.ecommerce.databinding.FragmentProductDetailsBinding
 
 class ProductDetailsFragment: Fragment() {
     private lateinit var viewModel: ProductDetailsViewModel
+    private lateinit var binding: FragmentProductDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProductDetailsBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(ProductDetailsViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_product_details, container, false)
+
+        return binding.root
     }
 
     companion object {
