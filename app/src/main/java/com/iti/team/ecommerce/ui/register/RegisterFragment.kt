4b5928c962a02db5fe1212cd00e016a47a274f4f
Nermeen Abdel Azeem <nermeen.abdel.azeem810@gmail.com>
2021-05-26 08:49:10 +0200

    register design and validation

--- app/src/main/java/com/iti/team/ecommerce/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/iti/team/ecommerce/ui/register/RegisterFragment.kt
@@ -4,24 +4,161 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.iti.team.ecommerce.R
+import com.iti.team.ecommerce.databinding.FragmentRegisterBinding
 import com.iti.team.ecommerce.ui.main.MainViewModel
 
 class RegisterFragment: Fragment() {
 
     private lateinit var viewModel: MainViewModel
-
+    private lateinit var binding:FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cirRegisterButton.setOnClickListener(View.OnClickListener {
+            registerUser()
+        })
     }
 
     companion object {
 
+    }
+
+    fun registerUser() {
+        if ( !validateFirstName() && !validateLastName() && !validatePassword() && !validateConfirmPass()&& !validatePhoneNo() && !validateEmail() && !validateAddrees()) {
+            Toast.makeText(context, "registerd", Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(context, "not registerd", Toast.LENGTH_LONG).show()
+
+        }
+    }
+    private fun validateFirstName(): Boolean {
+        val firstName: String = binding.editTextFirstName.getText().toString()
+        return if (firstName.isEmpty()) {
+            binding.textInputFirstName.setError("Field cannot be empty")
+          return false
+        } else {
+            binding.textInputFirstName.setError(null)
+            binding.textInputFirstName.setErrorEnabled(false)
+           return true
+        }
+    }
+    private fun validateLastName(): Boolean {
+        val lastName: String = binding.editTextLastName.getText().toString()
+        return if (lastName.isEmpty()) {
+            binding.textInputLastName.setError("Field cannot be empty")
+            return false
+        } else {
+            binding.textInputLastName.setError(null)
+            binding.textInputLastName.setErrorEnabled(false)
+            return true
+        }
+    }
+    private fun validateEmail(): Boolean {
+        val email: String = binding.editTextEmail.getText().toString()
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
+        return if (email.isEmpty()) {
+            binding.textInputEmail.setError("Field cannot be empty")
+            false
+        } else if (!email.matches(emailPattern)) {
+            binding.textInputEmail.setError("Invalid email address")
+            false
+        } else {
+            binding.textInputEmail.setError(null)
+            binding.textInputEmail.setErrorEnabled(false)
+            true
+        }
+    }
+    private fun validatePassword(): Boolean {
+        val pass: String = binding.editTextPassword.getText().toString()
+        val passwordVal = "^" +  //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +  //any letter
+                "(?=.*[@#$%^&+=])" +  //at least 1 special character
+                "(?=\\S+$)" +  //no white spaces
+                ".{4,}" +  //at least 4 characters
+                "$"
+        return if (pass.isEmpty()) {
+            binding.textInputPassword.setError("Field cannot be empty")
+            false
+        } else if (!pass.matches(passwordVal.toRegex())) {
+            binding.textInputPassword.setError("Password is too weak")
+            false
+        } else {
+            binding.textInputPassword.setError(null)
+            binding.textInputPassword.setErrorEnabled(false)
+            true
+        }
+    }
+    private fun validatePhoneNo(): Boolean {
+        val phone: String = binding.editTextMobile.getText().toString()
+        return if (phone.isEmpty()) {
+            binding.textInputMobile.setError("Field cannot be empty")
+            false
+        } else {
+            binding.textInputMobile.setError(null)
+            binding.textInputMobile.setErrorEnabled(false)
+            true
+        }
+    }
+    private fun validateConfirmPass(): Boolean {
+        val confirmPass: String = binding.editTextConfirmPassword.getText().toString()
+        return if (confirmPass.isEmpty()) {
+            binding.textInputConfirmPassword.setError("Field cannot be empty")
+            false
+        }
+        else if (!confirmPass.equals(binding.editTextPassword)) {
+            binding.textInputConfirmPassword.setError("Password not matching  ")
+            false
+        }
+        else {
+            binding.textInputConfirmPassword.setError(null)
+            binding.textInputConfirmPassword.setErrorEnabled(false)
+            true
+        }
+
+    }
+    private fun validateAddrees(): Boolean {
+        val street: String = binding.editTextStreet.getText().toString()
+        val city: String = binding.editTextCity.getText().toString()
+        val country: String = binding.editTextCountry.getText().toString()
+        var isValid:Boolean=false
+        if(!street.isEmpty()&&!city.isEmpty()&&!country.isEmpty()){
+            binding.textInputstreet.setError(null)
+            binding.textInputstreet.setErrorEnabled(false)
+            binding.textInputCity.setError(null)
+            binding.textInputCity.setErrorEnabled(false)
+            binding.textInputCountry.setError(null)
+            binding.textInputCountry.setErrorEnabled(false)
+            isValid=true
+        }
+        else {
+            if (street.isEmpty()) {
+                binding.textInputstreet.setError("Field cannot be empty")
+                isValid = false
+            }
+            if (city.isEmpty()) {
+                binding.textInputCity.setError("Field cannot be empty")
+                isValid = false
+            }
+            if (country.isEmpty()) {
+                binding.textInputCountry.setError("Field cannot be empty")
+                isValid = false
+            }
+
+        }
+        return isValid
     }
 }
