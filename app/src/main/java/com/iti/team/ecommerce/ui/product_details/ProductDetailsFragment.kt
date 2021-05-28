a4d14b4d0e10ae5fa517150a6db8e8ca0b5a1b1b
Wesam Elmeniawy <wesam.elmeniawy@gmail.com>
2021-05-28 16:00:54 +0200

    build image slider adapter

--- app/src/main/java/com/iti/team/ecommerce/ui/product_details/ProductDetailsFragment.kt
+++ app/src/main/java/com/iti/team/ecommerce/ui/product_details/ProductDetailsFragment.kt
@@ -1,22 +1,44 @@
 package com.iti.team.ecommerce.ui.product_details
 
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.iti.team.ecommerce.R
+import com.iti.team.ecommerce.databinding.FragmentProductDetailsBinding
+import com.iti.team.ecommerce.model.data_classes.Images
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
+import com.smarteist.autoimageslider.SliderAnimations
 
 class ProductDetailsFragment: Fragment() {
+
+    private lateinit var binding: FragmentProductDetailsBinding
     private lateinit var viewModel: ProductDetailsViewModel
+
+    private lateinit var sliderAdapter: SliderAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProductDetailsBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(ProductDetailsViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_product_details, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init(){
+       val images = listOf(
+           Images("https://cdn.shopify.com/s/files/1/0567/9310/4582/products/44694ee386818f3276566210464cf341.jpg?v=1621288163")
+           ,Images("https://cdn.shopify.com/s/files/1/0567/9310/4582/products/e8490702c423e6c62d356cace500822f.jpg?v=1621288163"),Images(""),Images(""))
+        sliderAdapter = SliderAdapter(images)
+        binding.imageSlider.setSliderAdapter(sliderAdapter)
+        binding.imageSlider.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        binding.imageSlider.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        binding.imageSlider.startAutoCycle();
     }
 
     companion object {
